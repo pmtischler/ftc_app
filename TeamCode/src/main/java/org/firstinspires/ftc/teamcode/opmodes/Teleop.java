ccfd26ecf8f69909d97a6e66a9f58b57ffa177a7
Phillip Tischler <pmtischler@gmail.com>
2019-11-17 23:32:09 -0500

    Remove optionality of gripper.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
@@ -28,26 +28,24 @@ public class Teleop extends Hardware {
         // Drive using Mecanum controls for gamepad 1.
         mecanum.setDriveFromGamepad(gamepad1);
 
-        if (gripper != null) {
-            // Open / close the gripper.
-            //   A     -> Close.
-            //   A + B -> Half open.
-            //       B -> Open.
-            if (gamepad1.a && gamepad1.b) {
-                gripper.openLeft();
-            } else if (gamepad1.a) {
-                gripper.close();
-            } else if (gamepad1.b) {
-                gripper.open();
-            }
-            // Rotate the gripper.
-            //   Y -> Front to back.
-            //   X -> Left to right.
-            if (gamepad1.y) {
-                gripper.rotateToFrontBack();
-            } else if (gamepad1.x) {
-                gripper.rotateToLeftRight();
-            }
+        // Open / close the gripper.
+        //   A     -> Close.
+        //   A + B -> Half open.
+        //       B -> Open.
+        if (gamepad1.a && gamepad1.b) {
+            gripper.openLeft();
+        } else if (gamepad1.a) {
+            gripper.close();
+        } else if (gamepad1.b) {
+            gripper.open();
+        }
+        // Rotate the gripper.
+        //   Y -> Front to back.
+        //   X -> Left to right.
+        if (gamepad1.y) {
+            gripper.rotateToFrontBack();
+        } else if (gamepad1.x) {
+            gripper.rotateToLeftRight();
         }
     }
 
